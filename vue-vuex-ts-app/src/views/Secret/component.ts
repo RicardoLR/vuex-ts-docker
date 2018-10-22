419dd84393622a7094c20c899e5b6f4bc1fb4284
Ricardo <rlopez@interware.com.mx>
2018-10-22 11:22:57 -0500

    uso de hooks y compute

--- vue-vuex-ts-app/src/views/Secret/component.ts
+++ vue-vuex-ts-app/src/views/Secret/component.ts
@@ -6,6 +6,9 @@ import {User} from '@/store/modules/auth/types';
 import AuthMixin from "@/mixins/AuthMixin";
 const namespace: string = 'authModule';
 
+/**
+ * Hooks para proteger ruta
+ */
 Component.registerHooks([
     'beforeRouteEnter'
 ]);
@@ -17,14 +20,25 @@ export default class Secret extends Vue {
     @State('isLogged', {namespace}) isLogged !: boolean;
     @State('user', {namespace}) user !: User;
     
+    /**
+     * los hooks no tienes contexto de la clase, ej this.isLoggedes undefined
+     */
     async beforeRouteEnter (from: Route, to: Route, next: any) {
         next((vm: Secret) => {
+
+            /** vm: MiClase, podemos acceder dentro a vm.miState etc 
+             * 
+             * Lo mas comveniente seria del Token tomar el rol
+            */
             if (vm.isLogged === false) {
                 next('/login');
             }
         })
     }
     
+    /**
+     * Propiedades computadas con GET 
+     */
     get fullUser (): string {
         return `Email: ${this.user.email}, Password: ${this.user.password}`;
     }
